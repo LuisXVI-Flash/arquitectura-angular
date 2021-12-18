9a80a8e65e384378008229168e81e1cfd777dc2b
LuisXVI-Flash <luisigitlab@fragote.com>
2021-12-18 00:31:16 -0500

    template listo

--- src/app/components/sidebar/dispositivos/dispositivos.component.ts
+++ src/app/components/sidebar/dispositivos/dispositivos.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispositivosComponent implements OnInit {
 
+  dtOptions: DataTables.Settings = {};
   constructor() { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pageLength: 10,
+      language: {
+        processing: "Procesando...",
+        search: "Buscar:",
+        lengthMenu: "Mostrar _MENU_ registros",
+        info: "Mostrando desde _START_ al _END_ de _TOTAL_ registros",
+        infoEmpty: "Mostrando ningún registro.",
+        infoFiltered: "(filtrado _MAX_ registros total)",
+        infoPostFix: "",
+        loadingRecords: "Cargando registros...",
+        zeroRecords: "No se encontraron registros",
+        emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        },
+        aria: {
+          sortAscending: ": Activar para ordenar la tabla en orden ascendente",
+          sortDescending: ": Activar para ordenar la tabla en orden descendente"
+        }
+      },
+      responsive: true
+    };
   }
 
 }
